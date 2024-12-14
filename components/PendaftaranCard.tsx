a3b71be8e1d3fd8100a093aed7e632af29131de4
Shawn <myking123321@gmail.com>
2024-12-14 22:51:17 +0700

    modified: page layout for daftar ulang pasien

--- components/PendaftaranCard.tsx
+++ components/PendaftaranCard.tsx
@@ -10,21 +10,60 @@ function PendaftaranCard({
 }) {
   return (
     <>
-      <div className="rounded-md bg-white p-4">
-        <p className="text-xl font-bold">{pendaftaran.nama_pasien}</p>
-        <p className="text-slate-400">
-          Id pendaftaran : {pendaftaran.id_pendaftaran}
-        </p>
-        <p className="text-slate-400">
-          No rekam medis : {pendaftaran.no_rkm_medis}
-        </p>
+      <div className="rounded-lg shadow-lg bg-white pl-6 pt-6 pr-4 pb-4 transition-transform transform hover:scale-95 hover:shadow-2xl">
 
-        <p className="font-bold">Dokter : {pendaftaran.nama_dokter}</p>
-        <p>Ruangan : {pendaftaran.no_ruang}</p>
-        <p>
-          {pendaftaran.start_time} - {pendaftaran.end_time}
-        </p>
-        {children}
+        <p className="text-2xl font-bold text-gray-800 border-b border-gray-400 ">{pendaftaran.nama_pasien}</p>
+        
+        
+        <div className="grid grid-cols-2 gap-6 mt-4 indent-4">
+          
+          <div className="flex flex-col">
+            <p className="text-sm text-gray-600 mb-2">
+              ID Pendaftaran:
+              <span className="font-semibold ml-2 text-gray-800">
+                {pendaftaran.id_pendaftaran}
+              </span>
+            </p>
+            <p className="text-sm text-gray-600">
+              Nomor Rekam Medis:
+              <span className="font-semibold ml-2 text-gray-800">
+                <br/>
+                <span className="indent-4">{pendaftaran.no_rkm_medis}</span>
+              </span>
+            </p>
+          </div>
+
+     
+          <div>
+            <p className="text-sm text-gray-600 mb-2">
+              Dokter:
+              <span className="font-semibold ml-2 text-gray-800">
+                {pendaftaran.nama_dokter}
+              </span>
+            </p>
+            <p className="text-sm text-gray-600">
+              Ruangan:
+              <span className="font-semibold ml-2 text-gray-800">
+                {pendaftaran.no_ruang}
+              </span>
+            </p>
+          </div>
+        </div>
+
+ 
+        <div className="border-t border-gray-200 my-4" />
+
+
+        <div className="font-bold indent-4">
+              <p>
+                Jadwal Praktek :
+              </p>
+              <p className="flex justify-between items-center text-m text-gray-500 mt-1">
+                {pendaftaran.start_time} - {pendaftaran.end_time}
+              </p>
+
+            </div>
+          {children}
       </div>
     </>
   );

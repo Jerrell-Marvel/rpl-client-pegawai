03a4a9af7fb817ba2d5dd3ac6f31a949c9261acc
Shawn <myking123321@gmail.com>
2024-12-15 13:18:45 +0700

    modified(riwayat-rekam-medis): style page a bit

--- app/riwayat-rekam-medis/[id_pasien]/page.tsx
+++ app/riwayat-rekam-medis/[id_pasien]/page.tsx
@@ -50,31 +50,40 @@ function RiwayatRekamMedisPage({ params }: RiwayatRekammedisPageProps) {
     <div>
       {rekamMedis?.map((rkm) => {
         return (
-          <div key={rkm.id_rkm_med}>
+          <div key={rkm.id_rkm_med} className="grid grid-cols-3">
+            <div className="flex flex-col gap-4 m-4">
+              <p>id_pendaftaran: {rkm.id_pendaftaran}</p>
+              <p>status: {rkm.status}</p>
+              <p>antrian: {rkm.antrian}</p>
+              <p>tanggal_daftar: {rkm.tanggal_daftar}</p>
+              <p>id_pasien: {rkm.id_pasien}</p>
+              <p>id_jadwal: {rkm.id_jadwal}</p>
+
+            </div>
             {/* Tampilin seperlunya aja nanti*/}
-            <p>id_pendaftaran: {rkm.id_pendaftaran}</p>
-            <p>status: {rkm.status}</p>
-            <p>antrian: {rkm.antrian}</p>
-            <p>tanggal_daftar: {rkm.tanggal_daftar}</p>
-            <p>id_pasien: {rkm.id_pasien}</p>
-            <p>id_jadwal: {rkm.id_jadwal}</p>
-            <p>id_rkm_med: {rkm.id_rkm_med}</p>
-            <p>resep_obat: {rkm.resep_obat ?? "-"}</p>
-            <p>
-              prognosis_tindakan_lanjut: {rkm.prognosis_tindakan_lanjut ?? "-"}
-            </p>
-            <p>diag_penunjang: {rkm.diag_penunjang ?? "-"}</p>
-            <p>pemeriksaan_fisik: {rkm.pemeriksaan_fisik ?? "-"}</p>
-            <p>pemeriksaan_penunjang: {rkm.pemeriksaan_penunjang ?? "-"}</p>
-            <p>riwayat_penyakit: {rkm.riwayat_penyakit ?? "-"}</p>
-            <p>keluhan: {rkm.keluhan ?? "-"}</p>
-            <p>tinggi_badan: {rkm.tinggi_badan ?? "-"}</p>
-            <p>berat_badan: {rkm.berat_badan ?? "-"}</p>
-            <p>golongan_darah: {rkm.golongan_darah ?? "-"}</p>
-            <p>diastolik: {rkm.diastolik ?? "-"}</p>
-            <p>sistolik: {rkm.sistolik ?? "-"}</p>
-            <p>denyut_nadi: {rkm.denyut_nadi ?? "-"}</p>
-          </div>
+            <div className="flex flex-col gap-4 m-4">
+              <p>tinggi_badan: {rkm.tinggi_badan ?? "-"}</p>
+              <p>berat_badan: {rkm.berat_badan ?? "-"}</p>
+              <p>golongan_darah: {rkm.golongan_darah ?? "-"}</p>
+              <p>diastolik: {rkm.diastolik ?? "-"}</p>
+              <p>sistolik: {rkm.sistolik ?? "-"}</p>
+              <p>denyut_nadi: {rkm.denyut_nadi ?? "-"}</p>
+            </div>
+            <div className="flex flex-col gap-4 m-4">
+              <p>id_rkm_med: {rkm.id_rkm_med}</p>
+              <p>resep_obat: {rkm.resep_obat ?? "-"}</p>
+              <p>
+                prognosis_tindakan_lanjut: {rkm.prognosis_tindakan_lanjut ?? "-"}
+              </p>
+              <p>diag_penunjang: {rkm.diag_penunjang ?? "-"}</p>
+              <p>pemeriksaan_fisik: {rkm.pemeriksaan_fisik ?? "-"}</p>
+              <p>pemeriksaan_penunjang: {rkm.pemeriksaan_penunjang ?? "-"}</p>
+              <p>riwayat_penyakit: {rkm.riwayat_penyakit ?? "-"}</p>
+              <p>keluhan: {rkm.keluhan ?? "-"}</p>
+
+            </div>
+
+            </div>
         );
       })}
     </div>

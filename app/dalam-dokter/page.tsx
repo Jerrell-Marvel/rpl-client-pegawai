6965ee3ae80b0fb3d1d2f76de2a09a8f7b1af70b
Shawn <myking123321@gmail.com>
2024-12-15 13:17:35 +0700

    modified(dalam-dokter): style page a bit

--- app/dalam-dokter/page.tsx
+++ app/dalam-dokter/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+
 import PemanggilanCard from "@/components/PemanggilanCard";
 import PendaftaranCard from "@/components/PendaftaranCard";
 import { Pemanggilan, Pendaftaran } from "@/types/pendaftaran";
 import { AxiosInstance } from "@/utils/axiosInstance";
-import { Button } from "@nextui-org/react";
+import { Button, ScrollShadow } from "@nextui-org/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 function PemanggilanPage() {
+  const current_date =  new Date();
   const [pendaftaran, setPendaftaran] = useState<Pemanggilan[]>([]);
 
   console.log(pendaftaran);
@@ -32,22 +34,38 @@ function PemanggilanPage() {
   }, []);
 
   return (
-    <main className="flex flex-col gap-8 p-12">
-      {pendaftaran.map((p) => {
-        return (
-          <>
-            <PemanggilanCard pemanggilan={p}>
-              <Button
-                as={Link}
-                className="bg-primary text-white"
-                href={`/diagnosis/${p.id_rkm_med}`}
-              >
-                Diagnosis
-              </Button>
-            </PemanggilanCard>
-          </>
-        );
-      })}
+    <main>
+      <div className="p-4 pl-8 bg-white border-b border-black shadow-md">
+        <p className="text-xs text-m text-gray-500" >{current_date.toDateString()}</p>
+        <h1 className="text-3xl font-bold text-gray-800 mt-2">Diagnosa Pasien</h1> 
+
+      </div>
+      <div className="flex flex-col gap-8 items-center bg-gray-50 py-4">
+        <ScrollShadow hideScrollBar className="max-w-[1200px] w-full max-h-[740px] overflow-y-auto grid sm:grid-cols-1 lg:grid-cols-2 " >
+        {pendaftaran.map((p) => {
+          return (
+            <>
+            <div className="my-[20px] mx-[20px]">
+              <PemanggilanCard pemanggilan={p}>
+                <div className="flex justify-end">
+                  <Button
+                    as={Link}
+                    className="bg-primary text-white"
+                    href={`/diagnosis/${p.id_rkm_med}`}
+                  >
+                    Diagnosis
+                  </Button>
+
+                </div>
+              </PemanggilanCard>
+
+            </div>
+            </>
+          );
+          })}
+        </ScrollShadow>
+        </div >
+       
     </main>
   );
 }

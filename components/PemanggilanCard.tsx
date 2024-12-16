af1169777ae2cd5abfe30c5a737aa65a94df0adf
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-12-16 23:00:35 +0700

    style: fix card hover

--- components/PemanggilanCard.tsx
+++ components/PemanggilanCard.tsx
@@ -1,4 +1,5 @@
 import { Pendaftaran } from "@/types/pendaftaran";
+import { formatTime } from "@/utils/time";
 import React from "react";
 
 type PendaftaranPemanggilan = {
@@ -13,59 +14,63 @@ type PemanggilanCardProps = {
 function PemanggilanCard({ pemanggilan, children }: PemanggilanCardProps) {
   return (
     <>
-      <div className="rounded-lg shadow-lg bg-white pl-6 pt-6 pr-4 pb-4 transition-transform transform hover:scale-95 hover:shadow-2xl">
-  <p className="text-2xl font-bold text-gray-800 border-b border-gray-400">{pemanggilan.nama_pasien}</p>
-  
-  <div className="grid grid-cols-2 gap-6 mt-4 indent-4">
-    <div className="flex flex-col">
-      <p className="text-sm text-gray-600 mb-2">
-        Antrian:
-        <span className="font-semibold ml-2 text-gray-800">
-          {pemanggilan.antrian}
-        </span>
-      </p>
-      <p className="text-sm text-gray-600 mb-2">
-        ID Pendaftaran:
-        <span className="font-semibold ml-2 text-gray-800">
-          {pemanggilan.id_pendaftaran}
-        </span>
-      </p>
-      <p className="text-sm text-gray-600">
-        No Rekam Medis:
-        <span className="font-semibold ml-2 text-gray-800">
-          {pemanggilan.no_rkm_medis}
-        </span>
-      </p>
-    </div>
+      <div className="transform rounded-lg bg-white pb-4 pl-6 pr-4 pt-6 shadow-lg transition-transform">
+        <p className="border-b border-gray-400 text-2xl font-bold text-gray-800">
+          {pemanggilan.nama_pasien}
+        </p>
 
-    <div>
-      <p className="text-sm text-gray-600 mb-2">
-        Dokter:
-        <span className="font-semibold ml-2 text-gray-800">
-          {pemanggilan.nama_dokter}
-        </span>
-      </p>
-      <p className="text-sm text-gray-600">
-        Ruangan:
-        <span className="font-semibold ml-2 text-gray-800">
-          {pemanggilan.no_ruang}
-        </span>
-      </p>
-    </div>
-  </div>
+        <div className="mt-4 grid grid-cols-2 gap-6 px-4">
+          <div className="flex flex-col">
+            <p className="mb-2 text-sm text-gray-600">
+              Antrian:
+              <span className="ml-2 font-semibold text-gray-800">
+                {pemanggilan.antrian}
+              </span>
+            </p>
+            <p className="mb-2 text-sm text-gray-600">
+              ID Pendaftaran:
+              <span className="ml-2 font-semibold text-gray-800">
+                {pemanggilan.id_pendaftaran}
+              </span>
+            </p>
+            <p className="text-sm text-gray-600">
+              No Rekam Medis:
+              <span className="ml-2 font-semibold text-gray-800">
+                {pemanggilan.no_rkm_medis}
+              </span>
+            </p>
+          </div>
 
-  <div className="border-t border-gray-200 my-4" />
+          <div>
+            <p className="mb-2 text-sm text-gray-600">
+              Dokter:
+              <span className="ml-2 font-semibold text-gray-800">
+                {pemanggilan.nama_dokter}
+              </span>
+            </p>
+            <p className="text-sm text-gray-600">
+              Ruangan:
+              <span className="ml-2 font-semibold text-gray-800">
+                {pemanggilan.no_ruang}
+              </span>
+            </p>
+          </div>
+        </div>
 
-  <div className="font-bold indent-4">
-    <p>Jadwal Praktek:</p>
-    <p className="flex justify-between items-center text-m text-gray-500 mt-1">
-      {pemanggilan.start_time} - {pemanggilan.end_time}
-    </p>
-  </div>
+        <div className="my-4 border-t border-gray-200" />
 
-  {children}
-</div>
+        <div className="indent-4 font-bold">
+          <p>
+            Jadwal Praktek:
+            <span className="text-m mt-1 flex items-center justify-between text-gray-500">
+              {formatTime(pemanggilan.start_time)} -{" "}
+              {formatTime(pemanggilan.end_time)}
+            </span>
+          </p>
+        </div>
 
+        {children}
+      </div>
     </>
   );
 }
